5bacc5300fc4e9457ca34e0c2474399e818dc1b1
Quân Anh Đặng <quananhdang@MacBook-Pro-cua-Quan.local>
2025-07-16 17:03:43 +0700

    cors bug

--- backend/main.py
+++ backend/main.py
@@ -25,7 +25,8 @@ app.add_middleware(
         "http://localhost:3000",
         "http://127.0.0.1:3000",
         "http://localhost:8000",
-        "https://edu.jackielino-dev.io.vn"
+        "https://edu.jackielino-dev.io.vn",
+        "*"
     ],
     allow_credentials=True,
     allow_methods=["*"],
